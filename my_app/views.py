0868b7e8f61f2711db91472f903be7a661366aa1
Ridwan <tuzoibsa@gmail.com>
2020-01-23 10:50:00 -0600

    Update views.py

--- my_app/views.py
+++ my_app/views.py
@@ -4,21 +4,20 @@ from django.shortcuts import render
 from requests.compat import quote_plus
 from . import models
 
-BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
-BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
-
+#url with quiry thats dynamically generated
+BASE_CRAIGSLIST_URL =  'https://minneapolis.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg' # actually stored in data pid as data-ids 
 
 def home(request):
     return render(request, 'base.html')
 
-
 def new_search(request):
     search = request.POST.get('search')
-    models.Search.objects.create(search=search)
-    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
-    response = requests.get(final_url)
+    models.Search.objects.create(search=search)     #creating search object to hold searched querys in db
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))#qoute pluse alows ther to be empth spaces in search
+    response = requests.get(final_url) #print(final_url)
     data = response.text
-    soup = BeautifulSoup(data, features='html.parser')
+    soup = BeautifulSoup(data, features='html.parser') #parse data to bs4 object 
 
     post_listings = soup.find_all('li', {'class': 'result-row'})
 
@@ -28,7 +27,8 @@ def new_search(request):
         post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
 
-        if post.find(class_='result-price'):
+            
+        if post.find(class_='result-price'): #for searching that dont have price
             post_price = post.find(class_='result-price').text
         else:
             post_price = 'N/A'
@@ -40,11 +40,13 @@ def new_search(request):
         else:
             post_image_url = 'https://craigslist.org/images/peace.jpg'
 
+
         final_postings.append((post_title, post_url, post_price, post_image_url))
 
-    stuff_for_frontend = {
+    stuff_for_frontend ={
         'search': search,
         'final_postings': final_postings,
     }
-
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
+
+    
